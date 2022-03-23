b66157dd7519ce75dc4262a03678d6221fe252b0
Red Wei <hevilhuy2@gmail.com>
2022-03-23 13:52:48 +0700

    Remove unused code.

--- ComponentLifecycle/TransferServices/DemonstrationTransferService.cs
+++ ComponentLifecycle/TransferServices/DemonstrationTransferService.cs
@@ -4,18 +4,5 @@ namespace ComponentLifecycle.TransferServices;
 
 public class DemonstrationTransferService
 {
-    //private string _log = "";
-
-    //public string Log
-    //{
-    //    get => _log;
-    //    set
-    //    {
-    //        _log = value;
-    //        OnLogChanged?.Invoke(this, EventArgs.Empty);
-    //    }
-    //}
-
-    //public event EventHandler OnLogChanged = new((sender, args) => { });
     public ObservableCollection<string> Logs { get; set; } = new();
 }
